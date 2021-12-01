87439c72d5474ba5f0db2dca1b8baeb2adede900
Riptide_mac <kyle1001001@gmail.com>
2021-12-01 10:30:37 -0800

    ghost meme

--- FaceDetection/HaarCascadeMethod/FaceDetector.py
+++ FaceDetection/HaarCascadeMethod/FaceDetector.py
@@ -32,12 +32,17 @@ grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #BGR not RGB
 
 # #face detection
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+# face_coordinates = trained_face_data.detectMultiScale(grayscaled_img, minNeighbors=5, outputRejectLevels = True)
+# faces = face_coordinates[0]
+# neighbours = face_coordinates[1]
+# weights = face_coordinates[2]
 print(face_coordinates)
 
 # # loop thru faces
 for (x,y,w,h) in face_coordinates:
     cv2.rectangle(img, (x,y), (x+w, y+h), (randrange(128,256), randrange(128,256), randrange(128,256)), 2)
 
+cv2.rectangle(img, (420, 69), (69, 420), (191, 64, 191), 8)
 # #Display
 windowName = 'Face Detector'
 cv2.namedWindow(windowName,cv2.WINDOW_NORMAL)
